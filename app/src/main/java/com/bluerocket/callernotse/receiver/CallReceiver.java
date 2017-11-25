e3bf62228e368566379217bada651b163f265876
nehaluddin.haider.mohammed <nehaluddin.haider.mohammed@intl.verizon.com>
2017-11-25 15:59:26 +0530

    CODE REFACTOR

--- app/src/main/java/com/bluerocket/callernotse/receiver/CallReceiver.java
+++ app/src/main/java/com/bluerocket/callernotse/receiver/CallReceiver.java
@@ -2,39 +2,45 @@ package com.bluerocket.callernotse.receiver;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Handler;
+import android.util.Log;
 
+import com.bluerocket.callernotse.db.AppDatabase;
+import com.bluerocket.callernotse.models.NoteModel;
 import com.bluerocket.callernotse.services.CustomFloatingViewService;
 
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by nehal on 11/24/2017.
  */
 
 public class CallReceiver extends PhoneCallReceiver {
-
+    private AppDatabase appDatabase;
+    private  List<NoteModel> itemAndPersonList;
 
     Context context;
 
     @Override
     protected void onIncomingCallStarted(final Context ctx, String number, Date start) {
-       // Toast.makeText(ctx, "Nehal Incoming Call" + number, Toast.LENGTH_LONG).show();
+        // Toast.makeText(ctx, "Nehal Incoming Call" + number, Toast.LENGTH_LONG).show();
 
 
         context = ctx;
 
         final Intent intent = new Intent(context, CustomFloatingViewService.class);
-      //  intent.putExtra("phone_no", number);
+        //  intent.putExtra("phone_no", number);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                     context.startForegroundService(intent);
-            }else
-                context.startService(intent);
+                } else
+                    context.startService(intent);
             }
         }, 1000);
         new Handler().postDelayed(new Runnable() {
@@ -43,12 +49,11 @@ public class CallReceiver extends PhoneCallReceiver {
 
                 context.stopService(intent);
             }
-        }, 10000);
+        }, 30000);
     }
 
     @Override
-    protected void onIncomingCallEnded(Context ctx, String number, Date start, Date end)
-    {
+    protected void onIncomingCallEnded(Context ctx, String number, Date start, Date end) {
 
         context = ctx;
 
@@ -65,21 +70,20 @@ public class CallReceiver extends PhoneCallReceiver {
     }
 
     @Override
-    protected void onOutgoingCallStarted(final Context ctx, String number, Date start)
-    {
+    protected void onOutgoingCallStarted(final Context ctx, String number, Date start) {
         //  Toast.makeText(ctx,"Nehal Incoming Call"+ number,Toast.LENGTH_LONG).show();
 
-        context =   ctx;
+        context = ctx;
 
         final Intent intent = new Intent(context, CustomFloatingViewService.class);
-      //  intent.putExtra("phone_no",number);
+        //  intent.putExtra("phone_no",number);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                     context.startForegroundService(intent);
-                }else
+                } else
                     context.startService(intent);
             }
         }, 1000);
@@ -88,14 +92,13 @@ public class CallReceiver extends PhoneCallReceiver {
             @Override
             public void run() {
 
-                    context.stopService(intent);
+                context.stopService(intent);
             }
-        }, 10000);
+        }, 30000);
     }
 
     @Override
-    protected void onOutgoingCallEnded(Context ctx, String number, Date start, Date end)
-    {
+    protected void onOutgoingCallEnded(Context ctx, String number, Date start, Date end) {
         // Toast.makeText(ctx,"Bye Bye"+ number,Toast.LENGTH_LONG).show();
 
         context = ctx;
@@ -106,11 +109,57 @@ public class CallReceiver extends PhoneCallReceiver {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                   context.stopService(intent);
+                context.stopService(intent);
 
             }
         }, 500);
     }
+
+    @Override
+    protected void onAsyncTaskCalled(Context ctx, String number) {
+        //    Toast.makeText(ctx,"async task called"+ number,Toast.LENGTH_LONG).show();
+
+
+      /*  public void deleteItem (NoteModel noteModel){
+            new NoteListViewModel.deleteAsyncTask(appDatabase).execute(noteModel);
+        }*/
+
+
+
+
+          class compareAsyncTask extends AsyncTask<NoteModel, Void, Void> {
+
+            private AppDatabase db;
+
+            compareAsyncTask(AppDatabase appDatabase) {
+                db = appDatabase;
+            }
+
+            @Override
+            protected Void doInBackground(final NoteModel... params) {
+                db.itemAndPersonModel().getAllNoteItems();
+                Log.d("all","doinbackgroudrunning");
+                return null;
+            }
+
+        }
+
+
+       /* appDatabase = AppDatabase.getDatabase(ctx);
+        itemAndPersonList = appDatabase.itemAndPersonModel().getAllNoteItems();
+
+        for (NoteModel element : itemAndPersonList) {
+            Date date = element.getBorrowDate();
+            String phone = element.getItemName();
+            String note = element.getPersonName();
+
+            if (number.equalsIgnoreCase(phone)){
+                Log.d("notes exist","yes");
+            }
+        }*/
+    }
+
+
 }
 
 
