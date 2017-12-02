c6b4800a8be7af39101c61fabebdd1bd162bac3b
nehaluddin.haider.mohammed <nehaluddin.haider.mohammed@intl.verizon.com>
2017-12-03 01:59:12 +0530

    runtime permission

--- app/src/main/java/com/bluerocket/callernotse/activities/HomeActivity.java
+++ app/src/main/java/com/bluerocket/callernotse/activities/HomeActivity.java
@@ -1,90 +1,52 @@
 package com.bluerocket.callernotse.activities;
 
-import android.Manifest;
-import android.annotation.TargetApi;
-import android.app.Activity;
-import android.app.AlertDialog;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.CoordinatorLayout;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.ashokvarma.bottomnavigation.BottomNavigationBar;
 import com.ashokvarma.bottomnavigation.BottomNavigationItem;
 import com.ashokvarma.bottomnavigation.TextBadgeItem;
 import com.bluerocket.callernotse.R;
-import com.bluerocket.callernotse.adapters.RecyclerViewAdapter;
 import com.bluerocket.callernotse.callhitory.CallLogActivity;
-import com.bluerocket.callernotse.widget.Fab;
 import com.bluerocket.callernotse.fragments.AddNoteFragment;
 import com.bluerocket.callernotse.fragments.CallerNotesListFragment;
 import com.bluerocket.callernotse.fragments.HomeFragment;
-import com.bluerocket.callernotse.viewmodel.NoteListViewModel;
+import com.bluerocket.callernotse.widget.Fab;
 import com.gordonwong.materialsheetfab.MaterialSheetFab;
 import com.gordonwong.materialsheetfab.MaterialSheetFabEventListener;
 
-public class HomeActivity extends AppCompatActivity implements View.OnClickListener,HomeFragment.OnFragmentInteractionListener, BottomNavigationBar.OnTabSelectedListener,AddNoteFragment.OnFragmentInteractionListener {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener, HomeFragment.OnFragmentInteractionListener, BottomNavigationBar.OnTabSelectedListener, AddNoteFragment.OnFragmentInteractionListener {
 
-    private NoteListViewModel viewModel;
-    private RecyclerViewAdapter recyclerViewAdapter;
-    private RecyclerView recyclerView;
-    public static final int MY_PERMISSIONS_REQUEST_WRITE_LOGS = 123;
-
-    private static final long RIPPLE_DURATION = 200;
-    private static final long SET_DURATION = 1000;
-
-    private ActionBarDrawerToggle drawerToggle;
-    private DrawerLayout drawerLayout;
-    private MaterialSheetFab materialSheetFab;
-    private int statusBarColor;
-
+    public Fab fab;
     @Nullable
     TextBadgeItem numberBadgeItem;
-
-   private Toolbar toolbar;
-
-   private CoordinatorLayout root;
-
-   private View contentHamburger;
-    Context context;
     BottomNavigationBar bottomNavigationBar;
     int lastSelectedPosition = 0;
-    public Fab fab;
-    private static final int REQUEST_CONTACT = 1011;
-
+    private MaterialSheetFab materialSheetFab;
+    private int statusBarColor;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_home);
-        context = HomeActivity.this;
-        checkPermission();
 
+        setupFab();
+        setupBottomDrawer();
 
-        /*setupActionBar();
-        setupDrawer();
-        setupFab();
-        setupTabs();*/
-        setupFab();
+        setupNotificationCannel();
         materialSheetFab.showFab();
 
+    }
+
+    private void setupNotificationCannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             final String channelId = getString(R.string.default_floatingview_channel_id);
             final String channelName = getString(R.string.default_floatingview_channel_name);
@@ -94,8 +56,10 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
                 manager.createNotificationChannel(defaultChannel);
             }
         }
-    //    toolbar=findViewById(R.id.toolbar);
-        root=findViewById(R.id.root);
+
+    }
+
+    public void setupBottomDrawer() {
         bottomNavigationBar = (BottomNavigationBar) findViewById(R.id.bottom_navigation_bar);
         bottomNavigationBar.clearAll();
 
@@ -106,22 +70,6 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
                 .setBackgroundColorResource(R.color.blue)
                 .setText("" + 0)
                 .setHideOnSelect(true);
-
-/*
-
-        if (toolbar != null) {
-            setSupportActionBar(toolbar);
-            getSupportActionBar().setTitle(null);
-        }
-*/
-
-     /*   fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AddNoteFragment()).commitAllowingStateLoss();
-            }
-        });*/
-
 
         bottomNavigationBar.setFab(fab);
         bottomNavigationBar.setMode(0);
@@ -137,57 +85,8 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-
-
-    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-    public void checkPermission()
-    {
-        int currentAPIVersion = Build.VERSION.SDK_INT;
-        if(currentAPIVersion>=android.os.Build.VERSION_CODES.M)
-        {
-            if (ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_CALL_LOG) != PackageManager.PERMISSION_GRANTED) {
-                if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) context, Manifest.permission.WRITE_CALL_LOG)) {
-                    AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
-                    alertBuilder.setCancelable(true);
-                    alertBuilder.setTitle("Permission necessary");
-                    alertBuilder.setMessage("Write calendar permission is necessary to write event!!!");
-                    alertBuilder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                        @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-                        public void onClick(DialogInterface dialog, int which) {
-                            ActivityCompat.requestPermissions((Activity)context, new String[]{Manifest.permission.WRITE_CALENDAR}, MY_PERMISSIONS_REQUEST_WRITE_LOGS);
-                        }
-                    });
-                    AlertDialog alert = alertBuilder.create();
-                    alert.show();
-
-                } else {
-                    ActivityCompat.requestPermissions((Activity)context, new String[]{Manifest.permission.WRITE_CALENDAR}, MY_PERMISSIONS_REQUEST_WRITE_LOGS);
-                }
-                return;
-            } else {
-                return ;
-            }
-        } else {
-            return ;
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_WRITE_LOGS:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                 //   writeCalendarEvent();
-                } else {
-                    //code for deny
-                }
-                break;
-        }
-    }
-
     @Override
     public void onTabSelected(int position) {
-
 
         lastSelectedPosition = position;
         setMessageText(position + " Tab Selected");
@@ -202,19 +101,21 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         switch (position) {
             case 0:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commitAllowingStateLoss();
-
                 break;
             case 1:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new CallerNotesListFragment()).commitAllowingStateLoss();
                 break;
             case 2:
-                startActivity(new Intent(this,CallLogActivity.class));
+                startActivity(new Intent(this, CallLogActivity.class));
                 break;
             case 3:
-                startActivity(new Intent(this,SettingsPrefActivity.class));
+                startActivity(new Intent(this, SettingsPrefActivity.class));
                 break;
         }
     }
+
+
+
     @Override
     public void onTabUnselected(int position) {
     }
@@ -225,7 +126,7 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void setMessageText(String messageText) {
-     //   message.setText(messageText);
+        //   message.setText(messageText);
     }
 
 
@@ -243,12 +144,9 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    /**
-     * Sets up the Floating action button.
-     */
     public void setupFab() {
 
-       fab= (Fab) findViewById(R.id.fab);
+        fab = (Fab) findViewById(R.id.fab);
         View sheetView = findViewById(R.id.fab_sheet);
         View overlay = findViewById(R.id.overlay);
 
@@ -301,9 +199,7 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AddNoteFragment()).commitAllowingStateLoss();
-           // Toast.makeText(this, R.string.sheet_item_pressed, Toast.LENGTH_SHORT).show();
-            materialSheetFab.hideSheet();
-        }
-
+        materialSheetFab.hideSheet();
+    }
 }
 
--- app/src/main/java/com/bluerocket/callernotse/fragments/HomeFragment.java
+++ app/src/main/java/com/bluerocket/callernotse/fragments/HomeFragment.java
@@ -1,16 +1,25 @@
 package com.bluerocket.callernotse.fragments;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.databinding.DataBindingUtil;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bluerocket.callernotse.R;
 import com.bluerocket.callernotse.callhitory.CallHistoryHelper;
@@ -19,6 +28,8 @@ import com.bluerocket.callernotse.databinding.FragmentHomeBinding;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class HomeFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
@@ -32,6 +43,10 @@ public class HomeFragment extends Fragment {
     private ArrayList<String> conTime;
     private ArrayList<String> conDate;
     private ArrayList<String> conType;
+    private static final int EXTERNAL_STORAGE_PERMISSION_CONSTANT = 100;
+    private static final int REQUEST_PERMISSION_SETTING = 101;
+    private boolean sentToSettings = false;
+    private SharedPreferences permissionStatus;
 
     private OnFragmentInteractionListener mListener;
 
@@ -70,20 +85,14 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        permissionStatus = getActivity().getSharedPreferences("permissionStatus", MODE_PRIVATE);
 
         conNames = new ArrayList<String>();
         conNumbers = new ArrayList<String>();
         conTime = new ArrayList<String>();
         conDate = new ArrayList<String>();
         conType = new ArrayList<String>();
-
-
-        Cursor curLog= CallHistoryHelper.getAllCallLogs(getActivity().getContentResolver());
-        setCallLogs(curLog);
-
-        for(String names: conNames){
-            System.out.println(names);
-        }
+        checkPermssionForReadLogs();
     }
 
     public void onButtonPressed(Uri uri) {
@@ -153,5 +162,129 @@ public class HomeFragment extends Fragment {
             conTime.add(duration);
         }
     }
+
+    private void checkPermssionForReadLogs() {
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_CALL_LOG) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.READ_CALL_LOG)) {
+
+                //Show Information about why you need the permission
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle("Need Storage Permission");
+                builder.setMessage("This app needs storage permission.");
+                builder.setPositiveButton("Grant", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+
+                        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_CALL_LOG}, EXTERNAL_STORAGE_PERMISSION_CONSTANT);
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.show();
+
+            } else if (permissionStatus.getBoolean(Manifest.permission.READ_CALL_LOG, false)) {
+                //Previously Permission Request was cancelled with 'Dont Ask Again',
+                // Redirect to Settings after showing Information about why you need the permission
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle("Need Storage Permission");
+                builder.setMessage("This app needs storage permission.");
+                builder.setPositiveButton("Grant", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                        sentToSettings = true;
+                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                        Uri uri = Uri.fromParts("package", getActivity().getPackageName(), null);
+                        intent.setData(uri);
+                        startActivityForResult(intent, REQUEST_PERMISSION_SETTING);
+                        Toast.makeText(getActivity(), "Go to Permissions to Grant Storage", Toast.LENGTH_LONG).show();
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.show();
+            } else {
+                //just request the permission
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, EXTERNAL_STORAGE_PERMISSION_CONSTANT);
+            }
+
+            SharedPreferences.Editor editor = permissionStatus.edit();
+            editor.putBoolean(Manifest.permission.READ_CALL_LOG, true);
+            editor.commit();
+
+        } else {
+
+            //You already have the permission, just go ahead.
+              proceedAfterPermission();
+        }
+    }
+
+    private void proceedAfterPermission() {
+        Cursor curLog= CallHistoryHelper.getAllCallLogs(getActivity().getContentResolver());
+        setCallLogs(curLog);
+
+        Toast.makeText(getActivity().getBaseContext(), "We got the Storage Permission", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == EXTERNAL_STORAGE_PERMISSION_CONSTANT) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //The External Storage Write Permission is granted to you... Continue your left job...
+                proceedAfterPermission();
+            } else {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.READ_CALL_LOG)) {
+                    //Show Information about why you need the permission
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                    builder.setTitle("Need Storage Permission");
+                    builder.setMessage("This app needs storage permission");
+                    builder.setPositiveButton("Grant", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+
+
+                            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_CALL_LOG}, EXTERNAL_STORAGE_PERMISSION_CONSTANT);
+
+
+                        }
+                    });
+                    builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
+                    builder.show();
+                } else {
+                    Toast.makeText(getActivity().getBaseContext(),"Unable to get Permission",Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_PERMISSION_SETTING) {
+            if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_CALL_LOG) == PackageManager.PERMISSION_GRANTED) {
+                //Got Permission
+                proceedAfterPermission();
+            }
+        }
+    }
+
+
 
 }
